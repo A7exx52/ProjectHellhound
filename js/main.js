6fc7a236b817fab1b186ba7fd4c86318cf4acdcb
HenriBDev <henribdev@gmail.com.br>
2023-01-28 22:27:30 -0300

    :sparkles: feat - Log menu fully functional

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-const MAIN_TEXTBOX = document.querySelector('main'),
-      MAIN_TEXTBOX_COMP_STYLE = window.getComputedStyle(MAIN_TEXTBOX),
+const MAIN_TEXTSECTION = document.querySelector('#textSection'),
+      MAIN_TEXTSECTION_COMP_STYLE = window.getComputedStyle(MAIN_TEXTSECTION),
+
+      MENU_MODAL_BG = document.querySelector('#menuModalBg'),
       MENU_MODAL = document.querySelector('#menuModal'),
       MENU_MODAL_COMP_STYLE = window.getComputedStyle(MENU_MODAL),
-      MENU_MODAL_BG = document.querySelector('#menuModalBg'),
+      MENU_MODAL_HEADER = MENU_MODAL.querySelector('#modalHeader'),
+      MENU_MODAL_MAIN = MENU_MODAL.querySelector("#modalMain"),
+      MENU_MODAL_FOOTER = MENU_MODAL.querySelector("#modalFooter")
+
       LOG_BUTTON = document.querySelector('#logButton'),
-      OPTIONS_BUTTON = document.querySelector('#optionsButton')
-      SCRIPT_ARRAY = [
+      OPTIONS_BUTTON = document.querySelector('#optionsButton'),
+
+      SCRIPT = [
             ["Well, hello, mister detective."], 
             ["How have you been?"], 
             ["You've had quite the ordeal lately haven't you?", "It makes for quite an excellent story."], 
@@ -15,6 +21,8 @@ const MAIN_TEXTBOX = document.querySelector('main'),
             ["Well, then, detective, you remember very well how it started, don't you? It was a night of stormy weather, when that case reached your hands..."],
             ['"Where in the name of everything holy does that much rain come from?!"', 'You were sitting on your table, mindlessly checking the notes of the last case you solved, when you heard your boss, the tall, slightly overweight, gray-haired commissioner Edmund "Mundie" Dunkirk, complain for the Nth time in just the last forty minutes.']
       ],
+      LOG = [],
+
       TEXT_SPEEDS = {
           'NORMAL': 0.055,
           'SURPRISE': 0.0025,
@@ -28,56 +36,58 @@ const MAIN_TEXTBOX = document.querySelector('main'),
 
 let awaitingInput = false,
     textBlockIterator = 0,
-    printIntervalLoop;
+    textAnimationLoop;
 
 function displayNextTextBlock(textSpeed){
     let textCharIterator = 0,
         textLineIterator = 0;
-    printIntervalLoop = setInterval(() => {
-        if(textLineIterator < SCRIPT_ARRAY[textBlockIterator].length){
-            if(textCharIterator < SCRIPT_ARRAY[textBlockIterator][textLineIterator].length){
-                MAIN_TEXTBOX.innerHTML += SCRIPT_ARRAY[textBlockIterator][textLineIterator][textCharIterator]
+    textAnimationLoop = setInterval(() => {
+        if(textLineIterator < SCRIPT[textBlockIterator].length){
+            if(textCharIterator < SCRIPT[textBlockIterator][textLineIterator].length){
+                MAIN_TEXTSECTION.innerHTML += SCRIPT[textBlockIterator][textLineIterator][textCharIterator]
                 textCharIterator++;
             }
             else{
                 textLineIterator++;
                 textCharIterator = 0;
-                MAIN_TEXTBOX.innerHTML += "<br>"
+                MAIN_TEXTSECTION.innerHTML += "<br>"
             }
         }else{
-            clearInterval(printIntervalLoop)
+            clearInterval(textAnimationLoop)
             awaitingInput = true;
+            LOG.push(SCRIPT[textBlockIterator])
             textBlockIterator += 1;
-            if(textBlockIterator == SCRIPT_ARRAY.length){
+            if(textBlockIterator == SCRIPT.length){
                 textBlockIterator = 0;
             }
         }
     }, textSpeed * 1000)
 }
 
-function cycleText(){
+function changeMainTextState(){
     if(ANIMATIONS_STATES['CHANGE_TEXT'] == 'NOT RUNNING'){
         ANIMATIONS_STATES['CHANGE_TEXT'] = 'RUNNING';
         if (awaitingInput){
             let cleanIntervalLoop = setInterval(function(){
-                MAIN_TEXTBOX.style.opacity = parseFloat(MAIN_TEXTBOX_COMP_STYLE.getPropertyValue('opacity')) - 0.1; 
+                MAIN_TEXTSECTION.style.opacity = parseFloat(MAIN_TEXTSECTION_COMP_STYLE.getPropertyValue('opacity')) - 0.1; 
             }, 50)
             setTimeout(() => {
                 clearInterval(cleanIntervalLoop);
-                MAIN_TEXTBOX.innerHTML= '';
-                MAIN_TEXTBOX.style.opacity = 1;
+                MAIN_TEXTSECTION.innerHTML= '';
+                MAIN_TEXTSECTION.style.opacity = 1;
                 displayNextTextBlock(TEXT_SPEEDS['NORMAL']);
                 awaitingInput = false;
                 ANIMATIONS_STATES['CHANGE_TEXT'] = 'NOT RUNNING';      
             }, 500)
         }else{
-            clearInterval(printIntervalLoop)
-            MAIN_TEXTBOX.innerHTML = SCRIPT_ARRAY[textBlockIterator][0]
-            for (let currentLine = 1; currentLine < SCRIPT_ARRAY[textBlockIterator].length; currentLine++){
-                MAIN_TEXTBOX.innerHTML += '<br>' + SCRIPT_ARRAY[textBlockIterator][currentLine] 
+            clearInterval(textAnimationLoop)
+            MAIN_TEXTSECTION.innerHTML = SCRIPT[textBlockIterator][0]
+            for (let currentLine = 1; currentLine < SCRIPT[textBlockIterator].length; currentLine++){
+                MAIN_TEXTSECTION.innerHTML += '<br>' + SCRIPT[textBlockIterator][currentLine] 
             }
+            LOG.push(SCRIPT[textBlockIterator])
             textBlockIterator += 1
-            if(textBlockIterator == SCRIPT_ARRAY.length){
+            if(textBlockIterator == SCRIPT.length){
                 textBlockIterator = 0
             }
             awaitingInput = true
@@ -86,12 +96,37 @@ function cycleText(){
     }
 }
 
-function toggleMenuModal(){
+function toggleMenuModal(menuType = null){
     if(ANIMATIONS_STATES['TOGGLE_MENU_MODAL'] == 'NOT RUNNING'){
         ANIMATIONS_STATES['TOGGLE_MENU_MODAL'] = 'RUNNING'
         if(MENU_MODAL_COMP_STYLE.getPropertyValue('display') == 'none'){
-            MENU_MODAL_BG.style.display = 'block'
-            MENU_MODAL.style.display = 'flex'
+            switch(menuType){
+                case "LOG":
+                    MENU_MODAL_HEADER.innerHTML = 'Log';
+                    MENU_MODAL_MAIN.innerHTML = '';
+                    MENU_MODAL_FOOTER.innerHTML = '';
+                    if (LOG.length >= 1){
+                        for (let currentBlock = 0; currentBlock < LOG.length; currentBlock++){
+                            MENU_MODAL_MAIN.innerHTML += '<span>'
+                            for (let currentLine = 0; currentLine < LOG[currentBlock].length; currentLine++){
+                                MENU_MODAL_MAIN.innerHTML += LOG[currentBlock][currentLine] + "<br>"
+                            }
+                            MENU_MODAL_MAIN.innerHTML += "</span>"
+                        }
+                        MENU_MODAL_MAIN.style.overflowY = "scroll";
+                        MENU_MODAL_MAIN.scrollTop = MENU_MODAL_MAIN.scrollHeight - 1
+                    }
+                    else{
+                        MENU_MODAL_MAIN.innerHTML = '';
+                    }
+                break;
+                case "OPTIONS":
+                    MENU_MODAL_HEADER.innerHTML = 'Options';
+                    MENU_MODAL_MAIN.innerHTML = '';
+                break;
+            }
+            MENU_MODAL_BG.style.display = 'block';
+            MENU_MODAL.style.display = 'flex';
             let cleanIntervalLoop = setInterval(function(){
                 MENU_MODAL.style.opacity = parseFloat(MENU_MODAL_COMP_STYLE.getPropertyValue('opacity')) + 0.1; 
             }, 15)
@@ -115,17 +150,32 @@ function toggleMenuModal(){
 
 menuIsVisible = () => MENU_MODAL_COMP_STYLE.getPropertyValue('display') != "none"
 
-MAIN_TEXTBOX.addEventListener('click', () => cycleText());
+MAIN_TEXTSECTION.addEventListener('click', () => changeMainTextState());
 
 window.addEventListener('keyup', function(e){
 
-    if((e.key == 'Enter' || e.key == ' ') && !menuIsVisible()) cycleText() 
+    switch(e.key){
 
-    else if(e.key == 'l' || e.key == 'o') toggleMenuModal()
+        case 'Enter':
+        case ' ':
+            if (!menuIsVisible()) changeMainTextState()
+        break;
+
+        case 'l':
+            toggleMenuModal('LOG')
+        break;
+
+        case 'o':
+            toggleMenuModal('OPTIONS')
+        break;
+
+    }
 
 });
 
-LOG_BUTTON.addEventListener("click", () => toggleMenuModal())
+LOG_BUTTON.addEventListener("click", () => toggleMenuModal('LOG'))
+
+OPTIONS_BUTTON.addEventListener("click", () => toggleMenuModal('LOG'))
 
 MENU_MODAL_BG.addEventListener("click", () => toggleMenuModal())
 
